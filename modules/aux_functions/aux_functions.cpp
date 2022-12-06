3f81e7b4af620db4319687f493c74abb518383f6
Matias Charrut <mcharrut@fi.uba.ar>
2022-12-06 13:44:15 -0300

    Fixed aux functions indentation.

--- modules/aux_functions/aux_functions.cpp
+++ modules/aux_functions/aux_functions.cpp
@@ -7,36 +7,35 @@
 #include "aux_functions.h"
 
 uint8_t floatIntegersCount(float number) {
-    int numberInt = number;
+  int numberInt = number;
 
-    uint8_t count = 0;
+  uint8_t count = 0;
 
-    if (numberInt == 0)
-        return 1;
+  if (numberInt == 0)
+    return 1;
 
-    if (numberInt < 0)
-        numberInt = -numberInt;
+  if (numberInt < 0)
+    numberInt = -numberInt;
 
-    while (numberInt > 0) {
-        numberInt /= 10;
-        count++;
-    }
+  while (numberInt > 0) {
+    numberInt /= 10;
+    count++;
+  }
 
-    return count;
+  return count;
 }
 
 std::string floatToString(float number, uint8_t precision) {
-    uint8_t length = (precision > 0) ? precision + 1 : 0;
-    length += floatIntegersCount(number);
-    length += (number < 0) ? 1 : 0;
+  uint8_t length = (precision > 0) ? precision + 1 : 0;
+  length += floatIntegersCount(number);
+  length += (number < 0) ? 1 : 0;
 
-    std::vector<char> output(length + 1);
-    char buffer_format[6];
+  std::vector<char> output(length + 1);
+  char buffer_format[6];
 
-    snprintf(buffer_format, sizeof(buffer_format), "%%.%uf", precision);
+  snprintf(buffer_format, sizeof(buffer_format), "%%.%uf", precision);
 
-    snprintf(&output[0], length + 1, buffer_format, number);
+  snprintf(&output[0], length + 1, buffer_format, number);
 
-    return std::string(&output[0]);
+  return std::string(&output[0]);
 }
-
