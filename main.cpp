682404be261f1916e9e8b065256759858e35e25d
Matias Charrut <mcharrut@fi.uba.ar>
2022-12-09 15:39:40 +0000

    Deleted macros from main and deleted sleep

--- main.cpp
+++ main.cpp
@@ -7,25 +7,9 @@
 #include "memory.h"
 #include "egardener.h"
 
-
-#define I2C_PORT2_SDA_PIN PB_9
-#define I2C_PORT2_SCL_PIN PB_8 
-
-#define WIFI_SERIAL_TX_PIN PD_5
-#define WIFI_SERIAL_RX_PIN PD_6
-#define WIFI_BAUDRATE 115200
-
-#define LIGHT_SENSOR_PIN A1
-
-#define ADDRESS_RTC 0x68
-#define ADDRESS_EEPROM 0x50
-#define ADDRESS_SENSOR_RH_TEMP 0x40
-
 UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
 
 int main() {
-    ThisThread::sleep_for(1s);
-
     eGardener eGardener;
 
     eGardener.execute();
